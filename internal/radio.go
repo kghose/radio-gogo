2c28577700337b53aba63dbd05a3f38eecfa5110
author <kaushik.ghose@gmail.com>
2024-04-09 23:17:02 -0400

    Cleanup: Remove obsolete code.

--- internal/radio.go
+++ internal/radio.go
@@ -6,25 +6,12 @@ import "fmt"
 
 type EventKind string
 
-const (
-	ERROR           EventKind = "ERROR"
-	DATA_REFRESHED  EventKind = "DATA_REFRESHED"
-	STATE_REFRESHED EventKind = "STATE_REFRESHED"
-)
-
-type Event struct {
-	kind    EventKind
-	message string
-}
-
 type Radio struct {
 	Servers        []Server
-	Tags           []Tag
 	Stations       []Station
 	CurrentStation Station
 	CurrentServer  Server
 	Error          string
-	Volume         int
 }
 
 type Server struct {
@@ -36,18 +23,6 @@ type Server struct {
 type Station struct {
 	Name string
 	Url  string
-}
-
-type Tag struct {
-	Name         string
-	Stationcount int
-}
-
-func (r *Radio) Init(radio_q chan Event) {
-	r.Servers = Get_list_of_available_servers()
-	radio_q <- Event{
-		STATE_REFRESHED,
-		fmt.Sprintf("%d Radio Browser servers found.", len(r.Servers))}
 }
 
 func (r *Radio) FindByTag(tag_list []string) {
@@ -64,8 +39,4 @@ func (r *Radio) FindByTag(tag_list []string) {
 
 func (r *Radio) Refresh_servers() {
 	r.Servers = Get_list_of_available_servers()
-}
-
-func (r *Radio) Refresh_tags() {
-	r.Tags = Get_tags(r.CurrentServer)
 }
