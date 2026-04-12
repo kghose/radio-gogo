8e0026dad96e7e12f7378dd38b55c1aee60b5237
Kaushik Ghose <kaushik.ghose@gmail.com>
2026-04-11 21:41:24 -0400

    Refactor out common code.

--- config.go
+++ config.go
@@ -2,7 +2,7 @@
 Where to save our program data and logs
 */
 
-package main 
+package main
 
 import (
 	"io/fs"
@@ -12,41 +12,38 @@ import (
 )
 
 const (
-	userDataDir  = "radio-gogo"
-	stationsFile = "stations.json"
-	logFile = "log.txt"
+	userDataDirName = "radio-gogo"
+	stationsFile    = "stations.json"
+	logFile         = "log.txt"
 )
 
-func stationsFilePath() (string, error) {
+func getUserDataDirPath() (string, error) {
 	home := os.Getenv("HOME")
 	dataHome := os.Getenv("XDG_DATA_HOME")
 	if dataHome == "" {
 		dataHome = path.Join(home, ".local", "share")
 	}
-	dataHome = path.Join(dataHome, userDataDir)
-	err := os.MkdirAll(dataHome, fs.FileMode(0777))
+	userDataDirPath := path.Join(dataHome, userDataDirName)
+	err := os.MkdirAll(userDataDirPath, fs.FileMode(0777))
 	if err != nil {
 		slog.Error("Unable to create data directory", "Error", err)
+		return "", err
 	}
+	return userDataDirPath, nil
+}
+
+func stationsFilePath() (string, error) {
+	userDataDirPath, err := getUserDataDirPath()
 	return path.Join(
-		dataHome,
+		userDataDirPath,
 		stationsFile,
 	), err
 }
 
 func logsFilePath() (string, error) {
-	home := os.Getenv("HOME")
-	dataHome := os.Getenv("XDG_DATA_HOME")
-	if dataHome == "" {
-		dataHome = path.Join(home, ".local", "share")
-	}
-	dataHome = path.Join(dataHome, userDataDir)
-	err := os.MkdirAll(dataHome, fs.FileMode(0777))
-	if err != nil {
-		slog.Error("Unable to create data directory", "Error", err)
-	}
+	userDataDirPath, err := getUserDataDirPath()
 	return path.Join(
-		dataHome,
+		userDataDirPath,
 		logFile,
 	), err
 }
